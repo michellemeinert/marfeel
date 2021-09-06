1c8742ba50d75a8cb786fd61a5d0946a55b9dd22
michellemeinert <michelle_meinert@yahoo.de>
2021-09-06 15:59:51 +0200

    missing graphs and responsiveness

--- js/main.js
+++ js/main.js
@@ -1,69 +1,92 @@
-'use strict';
+"use strict";
 const data = [
   {
-    color1: 'lightgreen',
-    color2: 'darkgreen',
-    headline: 'Revenue',
+    isEur: true,
+    color1: "--lightgreen",
+    color2: "--darkgreen",
+    headline: "REVENUE",
     headlineNumber: 200000,
     percentageTablet: 60,
     percentageSmartphone: 40,
   },
   {
-    color1: 'lightblue',
-    color2: 'darkblue',
-    headline: 'Impressions',
+    isEur: false,
+    color1: "--lightblue",
+    color2: "--darkblue",
+    headline: "IMPRESSIONS",
     headlineNumber: 50000000,
     percentageTablet: 40,
     percentageSmartphone: 60,
   },
   {
-    color1: 'lightyellow',
-    color2: 'orange',
-    headline: 'Visits',
+    isEur: false,
+    color1: "--lightyellow",
+    color2: "--orange",
+    headline: "VISITS",
     headlineNumber: 600000000,
     percentageTablet: 80,
     percentageSmartphone: 20,
   },
-]
+];
 
 let main = () => {
- const main = document.querySelector('main')
+  const main = document.querySelector("main");
 
- let buildSections = (html) => {
- main.innerHTML += html;
- }
- data.map(item => {
-  buildSections(`
-  <div class="is-wrapper is-centered-x is-column">
-  <div class="is-wrapper is-circle is-centered-x is-centered-y">
-    <div class="is-column has-text-align-center">
-      <h1>${item.headline}</h1>
-      <h1>${item.headlineNumber}</h1>
+  let buildSections = (html) => {
+    main.innerHTML += html;
+  };
+
+  let formatNumbers = (num) => {
+    return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+  };
+
+  data.map((item) => {
+    buildSections(`
+  <div class="is-wrapper is-centered-x is-centered-y is-column is-padding-14">
+  <div class="is-wrapper is-circle is-padding-4 is-centered-x is-centered-y" style="border-color: var(${
+    item.color1
+  })">
+    <div class="is-column is-text-align-center">
+      <h1 class="is-font-size-18 is-font-color-grey ">${item.headline}</h1>
+      <h1 class="is-font-size-26">${formatNumbers(item.headlineNumber)}${
+      item.isEur ? "€" : ""
+    }</h1>
     </div>
   </div>
   <div class="is-wrapper is-centered-y">
-    <div class="is-row is-wrapper">
+    <div class="is-row is-wrapper is-space-between is-width-300">
       <div class="is-column">
-        <h2>Tablet</h2>
-        <div class="is-row is-wrapper">
-          <p>${item.percentageTablet}%</p>
-          <p>${(item.headlineNumber*item.percentageTablet)/100}</p>
+        <h2 class="is-font-size-14" style="color: var(${
+          item.color1
+        })">Tablet</h2>
+        <div class="is-row is-wrapper is-padding-4 is-space-between">
+          <p class="is-font-size-14 is-margin-right-4">${
+            item.percentageTablet
+          }% </p>
+          <p class="is-font-color-grey is-font-size-14">${formatNumbers(
+            (item.headlineNumber * item.percentageTablet) / 100
+          )}${item.isEur ? "€" : ""}</p>
         </div>
       </div>
       <div class="is-column">
-        <h2>Smartphone</h2>
-        <div class="is-row is-wrapper">
-          <p>${item.percentageSmartphone}%</p>
-          <p>${((item.headlineNumber*item.percentageSmartphone)/100)}</p>
+        <h2 class="is-font-size-14"style="color: var(${
+          item.color2
+        }); text-align: end">Smartphone</h2>
+        <div class="is-row is-wrapper is-padding-4 is-space-between">
+          <p class="is-margin-right-4 is-font-size-14">${
+            item.percentageSmartphone
+          }% </p>
+          <p class="is-font-color-grey is-font-size-14">${formatNumbers(
+            (item.headlineNumber * item.percentageSmartphone) / 100
+          )}${item.isEur ? "€" : ""}</p>
         </div>
       </div>
     </div>
   </div>
-  <div class="is-line"></div>
+  <hr size="2" width="100%" color="grey">  
 </div>
-   `)
-})
-} 
+   `);
+  });
+};
 
-
-window.addEventListener('load', main);
+window.addEventListener("load", main);
