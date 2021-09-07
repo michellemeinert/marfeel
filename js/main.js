913ebf90cc8fe9bf226c2a0fa22763b630ec06a8
michellemeinert <michelle_meinert@yahoo.de>
2021-09-07 13:58:04 +0200

    diagrams and responsiveness fixed

--- js/main.js
+++ js/main.js
@@ -2,30 +2,30 @@
 const data = [
   {
     isEur: true,
-    color1: "--lightgreen",
-    color2: "--darkgreen",
+    colors: ["lightgreen", "darkgreen"],
     headline: "REVENUE",
     headlineNumber: 200000,
     percentageTablet: 60,
     percentageSmartphone: 40,
+    data: [120000, 80000],
   },
   {
     isEur: false,
-    color1: "--lightblue",
-    color2: "--darkblue",
+    colors: ["lightblue", "darkblue"],
     headline: "IMPRESSIONS",
     headlineNumber: 50000000,
     percentageTablet: 40,
     percentageSmartphone: 60,
+    data: [20000000, 30000000],
   },
   {
     isEur: false,
-    color1: "--lightyellow",
-    color2: "--orange",
+    colors: ["#FFC000", "	#FF4500"],
     headline: "VISITS",
     headlineNumber: 600000000,
     percentageTablet: 80,
     percentageSmartphone: 20,
+    data: [480000000, 120000000],
   },
 ];
 
@@ -43,22 +43,18 @@ let main = () => {
   data.map((item) => {
     buildSections(`
   <div class="is-wrapper is-centered-x is-centered-y is-column is-padding-14">
-  <div class="is-wrapper is-circle is-padding-4 is-centered-x is-centered-y" style="border-color: var(${
-    item.color1
-  })">
+  <canvas id=${item.headline} width="250" height="250">
     <div class="is-column is-text-align-center">
-      <h1 class="is-font-size-18 is-font-color-grey ">${item.headline}</h1>
+      <h1 class="is-font-size-18 is-font-color-grey">${item.headline}</h1>
       <h1 class="is-font-size-26">${formatNumbers(item.headlineNumber)}${
       item.isEur ? "€" : ""
     }</h1>
     </div>
-  </div>
+  </canvas>
   <div class="is-wrapper is-centered-y">
     <div class="is-row is-wrapper is-space-between is-width-300">
       <div class="is-column">
-        <h2 class="is-font-size-14" style="color: var(${
-          item.color1
-        })">Tablet</h2>
+        <h2 class="is-font-size-14" style="color: ${item.colors[0]}">Tablet</h2>
         <div class="is-row is-wrapper is-padding-4 is-space-between">
           <p class="is-font-size-14 is-margin-right-4">${
             item.percentageTablet
@@ -69,9 +65,9 @@ let main = () => {
         </div>
       </div>
       <div class="is-column">
-        <h2 class="is-font-size-14"style="color: var(${
-          item.color2
-        }); text-align: end">Smartphone</h2>
+        <h2 class="is-font-size-14"style="color: ${
+          item.colors[1]
+        }; text-align: end">Smartphone</h2>
         <div class="is-row is-wrapper is-padding-4 is-space-between">
           <p class="is-margin-right-4 is-font-size-14">${
             item.percentageSmartphone
@@ -86,6 +82,62 @@ let main = () => {
   <hr size="2" width="100%" color="grey">  
 </div>
    `);
+  });
+
+  let drawDonuts = (item) => {
+    let canvas = document.getElementById(item.headline);
+    let ctx = canvas.getContext("2d");
+    let total = item.headlineNumber;
+    let x = canvas.width / 2;
+    let y = canvas.height / 2;
+
+    let degreeToRadians = (angle) => {
+      return (angle * Math.PI) / 180;
+    };
+
+    let calculateEnd = (data, index, total) => {
+      return degreeToRadians(calculateEndAngle(data, index, total));
+    };
+
+    let calculateEndAngle = (data, index, total) => {
+      var angle = (data[index] / total) * 360;
+      var inc = index === 0 ? 0 : calculateEndAngle(data, index - 1, total);
+      return angle + inc;
+    };
+
+    let calculateStart = (data, index, total) => {
+      if (index === 0) {
+        return 0;
+      }
+      return calculateEnd(data, index - 1, total);
+    };
+
+    for (var i = 0; i < item.data.length; i++) {
+      ctx.beginPath();
+      ctx.arc(
+        x,
+        y,
+        120,
+        calculateStart(item.data, i, total),
+        calculateEnd(item.data, i, total)
+      );
+      ctx.lineWidth = 10;
+      ctx.font = "20px Arial";
+      ctx.textAlign = "center";
+      ctx.textBaseline = "middle";
+      ctx.fillText(item.headline, x, y - 10);
+      ctx.fillText(
+        formatNumbers(item.headlineNumber) + (item.isEur ? " €" : ""),
+        x,
+        y + 30
+      );
+      ctx.strokeStyle = item.colors[i];
+      ctx.stroke();
+    }
+  };
+
+  data.map((item) => {
+    drawDonuts(item);
   });
 };
 
